340e61e4ae0231bf0ca838f7f3bbc02a022f30c8
Unknown <michaelfami@gmail.com>
2018-03-02 13:33:21 -0600

    new

--- resources/views/contactme.blade.php
+++ resources/views/contactme.blade.php
@@ -1,7 +1,15 @@
 @extends('master')
+@section('head')
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" />
 
-@section('title','home')
-
+    <meta charset="utf-8">
+    <title>Home</title>
+</head>
+@endsection
 @section('content')
 <div class="container">
     <div class="row">
--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,120 +1,47 @@
-@extends('master')
+@extends('master') @section('head')
 
-@section('title','Portfolio')
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="http://anijs.github.io/lib/anicollection/anicollection.css">
 
-@section('content')
+    <meta charset="utf-8">
+    <title>Home</title>
+</head>
+@endsection @section('content')
 
-<div>
-  <!-- svg is defined here because of a Safari bug with <use> -->
-  <svg>
-    <defs>
-      <g id="p">
-        <path opacity=".2" d="M0 2h252s-3.3 40.7-2.7 128.3C249 216 252 272 252 272s-88-2-124.3-2C91.3 270 0 272 0 272V0z"/><path fill="#f4f3f1" d="M0 0h248v270H0z"/><path fill="#4f5150" d="M8 10h233v203H8z"/>
-      </g>
-      <g id="polaroid">
-        <use xlink:href="#p" transform="rotate(15)" x="175" y="50"/>
-        <use xlink:href="#p" transform="rotate(-5)" x="5" y="25"/>
-      </g>
-    </defs>
-  </svg>
-
-  <div class="demo__info">
-    Pure CSS parallax scrolling with navigation and CSS scrolling with scroll-behavior: smooth by Keith Clark. Please read the <a href="../">blog post</a> for more information.
-  </div>
-
-  <nav>
-    <ul class="nav">
-      <li><a href="#slide-1">Slide 1</a></li>
-      <li><a href="#slide-2">Slide 2</a></li>
-      <li><a href="#slide-3">Slide 3</a></li>
-      <li><a href="#slide-4">Slide 4</a></li>
-      <li><a href="#slide-5">Slide 5</a></li>
-      <li><a href="#slide-6">Slide 6</a></li>
-    </ul>
-  </nav>
-
-  <div class="slides">
-    <div class="slide" id="slide-1">
-      <div class="slide__bg"></div>
-      <div class="slide__content">
-        <div class="slide__text">
-          <h1 id="slide-1-title">Slide 1 title</h1>
-          <p>Bacon ipsum dolor amet alcatra beef meatloaf brisket beef ribs meatball tenderloin shank ball tip ribeye pig pastrami filet mignon. Sirloin tail fatback venison shank salami. Picanha pastrami venison meatball kevin pork chop leberkas. Meatloaf tenderloin leberkas pancetta hamburger cow kielbasa, filet mignon tri-tip beef ribs.</p>
-        </div>
-        <div class="slide__image">
-          <svg viewBox="0 0 400 425" width="400" height="425"><use xlink:href="#polaroid"/></svg>
-        </div>
-      </div>
+<div class="row text-white portfoliobox animated">
+    <div class="col-4 portdesc">
+        <h2>504 Sports</h2>
+        <p>Sports project.</p>
+        <button type="button" class="btn btn-primary" name="button">Next Project</button>
+    </div>
+    <div class="col-6">
+        <img id="portpic" src="/images/504screenshot.png" alt="">
     </div>
 
-    <div class="slide" id="slide-2">
-      <div class="slide__bg"></div>
-      <div class="slide__content">
-        <div class="slide__text">
-          <h1 id="slide-2-title">Slide 2 title</h1>
-          <p>Bacon ipsum dolor amet alcatra beef meatloaf brisket beef ribs meatball tenderloin shank ball tip ribeye pig pastrami filet mignon. Sirloin tail fatback venison shank salami. Picanha pastrami venison meatball kevin pork chop leberkas. Meatloaf tenderloin leberkas pancetta hamburger cow kielbasa, filet mignon tri-tip beef ribs.</p>
-          <p>Bacon ipsum dolor amet alcatra beef meatloaf brisket beef ribs meatball tenderloin shank ball tip ribeye pig pastrami filet mignon. Sirloin tail fatback venison shank salami. Picanha pastrami venison meatball kevin pork chop leberkas. Meatloaf tenderloin leberkas pancetta hamburger cow kielbasa, filet mignon tri-tip beef ribs.</p>
-        </div>
-        <div class="slide__image">
-          <svg viewBox="0 0 400 425" width="400" height="425"><use xlink:href="#polaroid"/></svg>
-        </div>
-      </div>
-    </div>
-
-    <div class="slide" id="slide-3">
-      <div class="slide__bg"></div>
-      <div class="slide__content">
-        <div class="slide__text">
-          <h1 id="slide-3-title">Slide 3 title</h1>
-          <p>Bacon ipsum dolor amet alcatra beef meatloaf brisket beef ribs meatball tenderloin shank ball tip ribeye pig pastrami filet mignon. Sirloin tail fatback venison shank salami. Picanha pastrami venison meatball kevin pork chop leberkas. Meatloaf tenderloin leberkas pancetta hamburger cow kielbasa, filet mignon tri-tip beef ribs.</p>
-          <p>Bacon ipsum dolor amet alcatra beef meatloaf brisket beef ribs meatball tenderloin shank ball tip ribeye pig pastrami filet mignon. Sirloin tail fatback venison shank salami. Picanha pastrami venison meatball kevin pork chop leberkas. Meatloaf tenderloin leberkas pancetta hamburger cow kielbasa, filet mignon tri-tip beef ribs.</p>
-        </div>
-        <div class="slide__image">
-          <svg viewBox="0 0 400 425" width="400" height="425"><use xlink:href="#polaroid"/></svg>
-        </div>
-      </div>
-    </div>
-
-    <div class="slide" id="slide-4">
-      <div class="slide__bg"></div>
-      <div class="slide__content">
-        <div class="slide__text">
-          <h1 id="slide-4-title">Slide 4 title</h1>
-          <p>Bacon ipsum dolor amet alcatra beef meatloaf brisket beef ribs meatball tenderloin shank ball tip ribeye pig pastrami filet mignon. Sirloin tail fatback venison shank salami. Picanha pastrami venison meatball kevin pork chop leberkas. Meatloaf tenderloin leberkas pancetta hamburger cow kielbasa, filet mignon tri-tip beef ribs.</p>
-        </div>
-        <div class="slide__image">
-          <svg viewBox="0 0 400 425" width="400" height="425"><use xlink:href="#polaroid"/></svg>
-        </div>
-      </div>
-    </div>
-
-    <div class="slide" id="slide-5">
-      <div class="slide__bg"></div>
-      <div class="slide__content">
-        <div class="slide__text">
-          <h1 id="slide-5-title">Slide 5 title</h1>
-          <p>Bacon ipsum dolor amet alcatra beef meatloaf brisket beef ribs meatball tenderloin shank ball tip ribeye pig pastrami filet mignon. Sirloin tail fatback venison shank salami. Picanha pastrami venison meatball kevin pork chop leberkas. Meatloaf tenderloin leberkas pancetta hamburger cow kielbasa, filet mignon tri-tip beef ribs.</p>
-        </div>
-        <div class="slide__image">
-          <svg viewBox="0 0 400 425" width="400" height="425"><use xlink:href="#polaroid"/></svg>
-        </div>
-      </div>
-    </div>
-
-    <div class="slide" id="slide-6">
-      <div class="slide__bg"></div>
-      <div class="slide__content">
-        <div class="slide__text">
-          <h1 id="slide-6-title">Slide 6 title</h1>
-          <p>Bacon ipsum dolor amet alcatra beef meatloaf brisket beef ribs meatball tenderloin shank ball tip ribeye pig pastrami filet mignon. Sirloin tail fatback venison shank salami. Picanha pastrami venison meatball kevin pork chop leberkas. Meatloaf tenderloin leberkas pancetta hamburger cow kielbasa, filet mignon tri-tip beef ribs.</p>
-        </div>
-        <div class="slide__image">
-          <svg viewBox="0 0 400 425" width="400" height="425"><use xlink:href="#polaroid"/></svg>
-        </div>
-      </div>
-    </div>
-
-  </div>
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+<script src="/js/anijs.js"></script>
+<script type="text/javascript">
+    $(".btn").on("click", function() {
+        var slide1content = "<h2>504 Sports</h2>"+"
+        <p>Sports project.</p>";
+        var slide2content =;
+        var slide3content = ;
+        var slide4content = ;
+
+        var slide1image ="/images/504screenshot.png";
+        var slide2image = ;
+        var slide3image = ;
+        var slide4image = ;
+
+        var slidestuff = [slide1content, slide2content, slide3content, slide4content];
+        var slidepics = [slide1image, slide2image, slide3image, slide4image];
+
+    });
+</script>
 @endsection
